6095962d4a5307452c178f2f08ceba2f5617d2d3
AnpengChen <alair.chan@gmail.com>
2014-10-15 15:16:46 +1100

    Update sort.java

--- sort.java
+++ sort.java
@@ -1,74 +1,74 @@
-import java.io.BufferedReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.Random;
+ Skip to content
+This repository
 
-class sort9323 {
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String string = br.readLine();
-		int seed = Integer.parseInt(string);
-		Random r = new Random(seed);
-		int[] array = new int[10000];
-		for (int i = 0; i < 10000; i++) {
-			array[i] = r.nextInt(10000);
-			// System.out.println(array[i]);
-		}
-		// quick sort
-		sort(array);
-		for (int i = 0; i < array.length; i++) {
-			// pw.println(array[i]);
-			System.out.println(array[i]);
-		}
-	}
+    Explore
+    Gist
+    Blog
+    Help
 
-	public static void sort(int[] inputArr) {
-		if (inputArr == null || inputArr.length == 0) {
-			return;
-		}
-		int length = inputArr.length;
-		quickSort(0, length - 1, inputArr);
-	}
+    JesseJia JesseJia
 
-	private static void quickSort(int lowerIndex, int higherIndex, int[] array) {
-		int i = lowerIndex;
-		int j = higherIndex;
-		// calculate pivot number, I am taking pivot as middle index number
-		int pivot = array[lowerIndex + (higherIndex - lowerIndex) / 2];
-		// Divide into two arrays
-		while (i <= j) {
-			/**
-			 * In each iteration, we will identify a number from left side which
-			 * is greater then the pivot value, and also we will identify a
-			 * number from right side which is less then the pivot value. Once
-			 * the search is done, then we exchange both numbers.
-			 */
-			while (array[i] < pivot) {
-				i++;
-			}
-			while (array[j] > pivot) {
-				j--;
-			}
-			if (i <= j) {
-				exchangeNumbers(i, j, array);
-				// move index to next position on both sides
-				i++;
-				j--;
-			}
-		}
-		// call quickSort() method recursively
-		if (lowerIndex < j)
-			quickSort(lowerIndex, j, array);
-		if (i < higherIndex)
-			quickSort(i, higherIndex, array);
-	}
+    1
+    0
+    0
 
-	private static void exchangeNumbers(int i, int j, int[] array) {
-		int temp = array[i];
-		array[i] = array[j];
-		array[j] = temp;
-	}
+JesseJia/bubbleSort
+
+bubbleSort / bubbleSort.java
+JesseJia JesseJia 3 days ago
+Update bubbleSort.java
+
+1 contributor
+46 lines (38 sloc) 1.019 kb
+/* package whatever; // don't place package name! */
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+/* Name of the class has to be "Main" only if the class is public. */
+class bubbleSort
+{
+public static void main (String[] args) throws IOException
+{
+// TODO Auto-generated method stub
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+String string = br.readLine();
+int seed = Integer.parseInt(string);
+Random r =new Random(seed);
+int[] array = new int[10000];
+for (int i = 0; i < 10000; i++) {
+array[i]=r.nextInt(10000);
+//System.out.println(array[i]);
 }
+//bubble sort
+bubbleSort(array);
+}
+public static void bubbleSort(int[] array) {
+//bubble sort
+for (int i = 0; i < array.length; i++) {
+for (int j = i+1; j < array.length; j++) {
+if (array[i]>array[j]) {
+int temp = array[i];
+array[i] = array[j];
+array[j] = temp;
+}
+}
+}
+for (int i = 0; i < array.length; i++) {
+System.out.println(array[i]);
+}
+}
+}
+
+    Status
+    API
+    Training
+    Shop
+    Blog
+    About
+
+    © 2014 GitHub, Inc.
+    Terms
+    Privacy
+    Security
+    Contact
+
